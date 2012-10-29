d97397809dddf7bb86c1b824b9d739af0d8353e6
Andi Altendorfer <andi@iboard.cc>
2012-10-29 21:28:11 +0100

    Add rvm-capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,10 +11,10 @@ set :branch, "master"
 set :rvm_ruby_string, '1.9.3-p194@r32'
 set :default_environment, {
     'PATH' => "/usr/local/rvm/gems/ruby-1.9.3-p194@r32/bin:/usr/local/rvm/gems/ruby-1.9.3-p194@global/bin:/usr/local/rvm/rubies/ruby-1.9.3-p194/bin:/usr/local/rvm/bin:$PATH",
-    'RUBY_VERSION' => 'ruby 1.9.3',
-    'GEM_HOME'     => '/usr/local/rvm/gems/ruby-1.9.3-p194@r32',
-    'GEM_PATH'     => '/usr/local/rvm/gems/ruby-1.9.3-p194@r32:/usr/local/rvm/gems/ruby-1.9.3-p194@global',
-    'BUNDLE_PATH'  => '/usr/local/rvm/gems/ruby-1.9.3-p194@r32'  # If you are using bundler.
+    'RUBY_VERSION' => 'ruby 1.9.3-p194',
+    'GEM_HOME'     => '/usr/local/rvm/gems/ruby-1.9.3-p194\@r32',
+    'GEM_PATH'     => '/usr/local/rvm/gems/ruby-1.9.3-p194\@r32:/usr/local/rvm/gems/ruby-1.9.3-p194\@global',
+    'BUNDLE_PATH'  => '/usr/local/rvm/gems/ruby-1.9.3-p194\@r32'  # If you are using bundler.
 }
 
 # END SETUP SECTION =============================================
